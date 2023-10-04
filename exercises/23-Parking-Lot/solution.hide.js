f0d109cc6f6cc7c339b4662a499cad70004bb140
Alejandro Barraza <71998273+alejandroBarraza@users.noreply.github.com>
2023-10-04 15:08:00 +0000

    refactor(solution.hide.js) : for loop refactor

--- exercises/23-Parking-Lot/solution.hide.js
+++ exercises/23-Parking-Lot/solution.hide.js
@@ -7,25 +7,26 @@ let parkingState = [
 
 // Your code here
 function getParkingLotState(parkingArray) {
-    let state = {
-      totalSlots: 0,
-      availableSlots: 0,
-      occupiedSlots: 0
-    }
-    for (let i = 0; i < parkingArray.length; i++) {
-      for (let x = 0; x < parkingArray[i].length; x++) {
-        if (parkingArray[i][x] === 2) {
-
-          state.availableSlots += 1
-          state.totalSlots += 1
-        }
-        else if (parkingArray[i][x] === 1) {
-          state.occupiedSlots += 1
-          state.totalSlots += 1
-        }
+  let state = {
+    totalSlots: 0,
+    availableSlots: 0,
+    occupiedSlots: 0
+  }
+  
+  for (const row of parkingArray) {
+    for (const column of row) {
+      if(column == 1){
+        state.occupiedSlots += 1
+        state.totalSlots += 1
+      }else if(column ===2){
+        state.availableSlots += 1
+        state.totalSlots += 1
       }
     }
-    return state
   }
+
+  return state
+}
+    
 
 console.log(getParkingLotState(parkingState));
